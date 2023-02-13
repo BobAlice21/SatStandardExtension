6f7e9fd81f9d83315e017225317ab4a7645c35e7
BobAlice21 <122107377+BobAlice21@users.noreply.github.com>
2023-02-13 16:05:35 +0100

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,40 +1,24 @@
-// Define a function that converts a currency amount to its equivalent value in bitcoin
-async function convertToBitcoin(amount, currency) {
-  // Fetch the current bitcoin price in the specified currency
-  const response = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`);
+// content_script.js
+
+// Function to retrieve the current exchange rate between bitcoin and the original currency
+async function convertToBitcoin(price, currency) {
+  const url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`;
+  const response = await fetch(url);
   const data = await response.json();
-  
-  // Extract the bitcoin price from the API response
-  const bitcoinPrice = data.bpi[currency].rate_float;
-  
-  // Convert the amount to bitcoin and return the result
-  return amount / bitcoinPrice;
+  const rate = data.bpi[currency].rate_float;
+  return price / rate;
 }
 
-// Inject a script into the webpage
-browser.tabs.executeScript({
-  code: `
-    // Find all elements on the page that contain currency amounts or prices
-    // const elements = document.querySelectorAll(".currency-amount");
-    const elements = document.querySelectorAll(".price");
+// Function to replace the original prices with the converted bitcoin prices
+async function replacePrices() {
+  const elements = document.querySelectorAll(".price");
+  for (let element of elements) {
+    const price = parseFloat(element.textContent.replace(",", ""));
+    const currency = "USD";
+    const bitcoinPrice = await convertToBitcoin(price, currency);
+    element.textContent = bitcoinPrice.toFixed(8) + " BTC";
+  }
+}
 
-    
-    // Convert each currency amount to its equivalent value in bitcoin
-    for (const element of elements) {
-      // Extract the currency amount from the element's text content
-      const match = element.textContent.match(/\d+(?:\.\d+)?/);
-      if (match) {
-        const amount = parseFloat(match[0]);
-        
-        // Determine the currency of the amount based on the element's class name
-        const currency = element.className.match(/currency-(\w+)/)[1];
-        
-        // Convert the amount to bitcoin
-        convertToBitcoin(amount, currency).then(result => {
-          // Replace the original currency amount with the converted bitcoin amount
-          element.textContent = result + " BTC";
-        });
-      }
-    }
-  `
-});
+// Call the replacePrices function when the page loads
+replacePrices();
